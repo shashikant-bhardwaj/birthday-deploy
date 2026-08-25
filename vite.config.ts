6d76621e50682130df73f5d9afbfa9c82a31d22a
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-08-25 21:24:56 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/birthday-deploy",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
